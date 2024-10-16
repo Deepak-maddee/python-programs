9d30053a171e3bd70ddfe0690ba4926d25b5b122
Deepak-maddee <maddee329@gmail.com>
2024-10-17 00:10:35 +0530

    Concession Store program
    This is a python program for creating a concession store which we find them in the malls and theaters. I basically have used dictionaries and have shown the importance of key:value pairs

---
+++ dict_prtc.py
@@ -0,0 +1,57 @@
+#dictionary
+#concession stand program
+
+menu = {"popcorn" : 150,
+    "soda" : 100,
+    "samosa" : 50,
+    "chips" : 60,
+    "burgers" : 75,
+    "pizza" : 100}
+
+cart = []
+total = 0
+
+print("----------MENU----------")
+for key,value in menu.items():
+    print(f"{key:10} : ${value:.2f}")
+
+print("------------------------")
+
+while True:
+    food = input("Enter the item you wish to buy and eat: ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+
+for food in cart:
+    total+=menu.get(food)
+    print(food,end = " ")
+
+print()
+print("----------YOUR ORDER----------")
+print(f"Total is: {total:.2f}")
+
+
+"""
+cap = {"KA" : "BG",
+    "TN" : "CH",
+    "AP" : "HYD"}
+
+
+print(dir(cap))
+print(help(cap))
+
+print(cap.get("KA"))
+
+if cap.get("KA"):
+    print("Exists")
+else:
+    print("NE")
+
+
+keys = cap.keys()
+for value in cap.values():
+    print(value)
+"""
