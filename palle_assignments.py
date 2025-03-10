73fcfb74d2bf75924da69d3ff05c1eb20a1c53d3
Deepak-maddee <maddee329@gmail.com>
2025-03-10 20:40:07 +0530

    made some changes

---
+++ palle_assignments.py
@@ -0,0 +1,18 @@
+#assignment 1
+
+_ = int(input("Enter a number to know the respective order of the days of the week in between 1-7: "))
+
+if _ == 1:
+    print("Monday")
+elif _ == 2:
+    print("Tuesday")
+elif _ == 3:
+    print("Wednesday")
+elif _ == 4:
+    print("Thursday")
+elif _ == 5:
+    print("Friday")
+elif _ == 6:
+    print("Saturday")
+else:
+    print("Sunday")
